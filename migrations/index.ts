e5a0724d60ead3e09e100d45371798418a82a956
greenisthenewblack <chrisg@gotbot.ai>
2026-04-30 13:37:29 +0200

    Migrate

---
+++ migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260430_113547 from './20260430_113547';
+
+export const migrations = [
+  {
+    up: migration_20260430_113547.up,
+    down: migration_20260430_113547.down,
+    name: '20260430_113547'
+  },
+];
