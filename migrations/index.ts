d4a94faace2bfd7e015a18f9caf82a3d5fee1e6d
greenisthenewblack <chrisg@gotbot.ai>
2026-05-01 10:31:12 +0200

    test

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,21 +1,9 @@
-import * as migration_20260430_143351 from './20260430_143351'
-import * as migration_20260430_143352 from './20260430_143352'
-import * as migration_20260430_143353 from './20260430_143353'
+import * as migration_20260501_080655 from './20260501_080655';
 
 export const migrations = [
   {
-    up: migration_20260430_143351.up,
-    down: migration_20260430_143351.down,
-    name: '20260430_143351',
+    up: migration_20260501_080655.up,
+    down: migration_20260501_080655.down,
+    name: '20260501_080655'
   },
-  {
-    up: migration_20260430_143352.up,
-    down: migration_20260430_143352.down,
-    name: '20260430_143352',
-  },
-  {
-    up: migration_20260430_143353.up,
-    down: migration_20260430_143353.down,
-    name: '20260430_143353',
-  },
-]
+];
