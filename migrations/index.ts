7bed1a16528fd99d3ec35eb7f483923ba9da4a3a
Vaughn Cleminson <vaughn@pulsesoftware.co.za>
2026-05-10 11:46:41 +0200

    Add migration for payload_jobs

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20260501_080655 from './20260501_080655';
 import * as migration_20260502_062500 from './20260502_062500';
 import * as migration_20260503_133320 from './20260503_133320';
+import * as migration_20260510_094405_add_payload_jobs_tables from './20260510_094405_add_payload_jobs_tables';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20260503_133320.up,
     down: migration_20260503_133320.down,
-    name: '20260503_133320'
+    name: '20260503_133320',
+  },
+  {
+    up: migration_20260510_094405_add_payload_jobs_tables.up,
+    down: migration_20260510_094405_add_payload_jobs_tables.down,
+    name: '20260510_094405_add_payload_jobs_tables'
   },
 ];
