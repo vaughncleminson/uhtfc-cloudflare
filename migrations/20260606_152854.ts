58d9f7342d90e174806ec88d2a44e14fbf8e3640
greenisthenewblack <chrisg@gotbot.ai>
2026-06-06 20:16:01 +0200

    Update 20260606_152854.ts

--- migrations/20260606_152854.ts
+++ migrations/20260606_152854.ts
@@ -1,15 +1,5 @@
-import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-d1-sqlite'
+import { MigrateDownArgs, MigrateUpArgs } from '@payloadcms/db-d1-sqlite'
 
-export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
-  await db.run(sql`ALTER TABLE \`locations\` DROP COLUMN \`contact_person\`;`)
-  await db.run(sql`ALTER TABLE \`locations\` DROP COLUMN \`contact_person_email\`;`)
-  await db.run(sql`ALTER TABLE \`_locations_v\` DROP COLUMN \`version_contact_person\`;`)
-  await db.run(sql`ALTER TABLE \`_locations_v\` DROP COLUMN \`version_contact_person_email\`;`)
-}
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {}
 
-export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
-  await db.run(sql`ALTER TABLE \`locations\` ADD \`contact_person\` text;`)
-  await db.run(sql`ALTER TABLE \`locations\` ADD \`contact_person_email\` text;`)
-  await db.run(sql`ALTER TABLE \`_locations_v\` ADD \`version_contact_person\` text;`)
-  await db.run(sql`ALTER TABLE \`_locations_v\` ADD \`version_contact_person_email\` text;`)
-}
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {}
