5225e64a4a871093c670bdf31e5c844f3db4efb4
greenisthenewblack <chrisg@gotbot.ai>
2026-09-07 15:04:19 +0200

    more

--- src/admin/components/Mailsend/sendBulkMail.tsx
+++ src/admin/components/Mailsend/sendBulkMail.tsx
@@ -22,21 +22,21 @@ export function SendBulkMail(props: any) {
       // const sentFrom = new Sender('uhtfc.office@gmail.com', 'UHTFC')
 
       // const recipients = [new Recipient('chris.trevor.green@gmail.com', 'Chris')]
-      const personalization = [
-        {
-          email: 'chrisg@codified.co.za',
-          data: {
-            recipientName: 'Chris Codified',
-            emailSubject: 'UHTFC AGM - 26th September 2025',
-            messageTitle: 'Please Join Us for the 71st UHTFC AGM',
-            messageBody: `
-            <p>Minutes and agenda for the 71st annual general meeting of the Underberg-Himeville Trout Fishing Club to be held at the Underberg Bowls Club on Saturday the 26th September 2026 at 19H00.</p>
-            <p>If you cannot attend, please find a link to the proxy letter below:</p>
-            <a href="https://uhtfc.s3.af-south-1.amazonaws.com/Proxy+letter+-+2026.docx" target="_blank">UHTFC AGM Proxy Letter</a>
-            `,
-          },
-        },
-      ]
+      // const personalization = [
+      //   {
+      //     email: 'chrisg@codified.co.za',
+      //     data: {
+      //       recipientName: 'Chris Codified',
+      //       emailSubject: 'UHTFC AGM - 26th September 2025',
+      //       messageTitle: 'Please Join Us for the 71st UHTFC AGM',
+      //       messageBody: `
+      //       <p>Minutes and agenda for the 71st annual general meeting of the Underberg-Himeville Trout Fishing Club to be held at the Underberg Bowls Club on Saturday the 26th September 2026 at 19H00.</p>
+      //       <p>If you cannot attend, please find a link to the proxy letter below:</p>
+      //       <a href="https://uhtfc.s3.af-south-1.amazonaws.com/Proxy+letter+-+2026.docx" target="_blank">UHTFC AGM Proxy Letter</a>
+      //       `,
+      //     },
+      //   },
+      // ]
       // const emailParams = new EmailParams()
       //   .setFrom(sentFrom)
       //   .setTo(recipients)
